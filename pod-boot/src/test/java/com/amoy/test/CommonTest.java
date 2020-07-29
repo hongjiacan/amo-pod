90d5303a6d6e311d97dbbd2d50d9f18e0e7fbcde
hongjiacan <littlepoemlittlewhite@gmail.com>
2020-07-29 23:48:52 +0800

    web

--- pod-boot/src/test/java/com/amoy/test/CommonTest.java
+++ pod-boot/src/test/java/com/amoy/test/CommonTest.java
@@ -4,6 +4,9 @@ import com.amoy.pod.support.utils.SequenceUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * TODO
  *
@@ -16,7 +19,34 @@ public class CommonTest {
     @Test
     public void test01(){
 
-        String redisKey = String.format("jwt-%d-%d", SequenceUtil.nextId(), "wulvping".hashCode());
-        log.info(redisKey);
+        String str = "123456789";
+        log.info(str.substring(1));
+    }
+
+    @Test
+    public void test02(){
+        List<String> seqList = new ArrayList<>();
+        for(int i = 1; i < 1000; i++){
+            if(i<10){
+                seqList.add("00"+i);
+            }else if(i<100){
+                seqList.add("0"+i);
+            }else{
+                seqList.add(String.valueOf(i));
+            }
+        }
+        log.info(seqList.toString());
+    }
+
+    @Test
+    public void test03(){
+
+        int a = 17;
+        int b = 16;
+
+        log.info(Integer.toBinaryString(a)
+                + " & " +
+                Integer.toBinaryString(b)
+                + " = " + String.valueOf(a&b));
     }
 }
--- pod-web/README.md
+++ pod-web/README.md
@@ -1,24 +1,39 @@
-# pod-web
+## Getting started
 
-## Project setup
-```
+```bash
+# install dependency
 npm install
+
+# develop
+npm run dev
 ```
 
-### Compiles and hot-reloads for development
-```
-npm run serve
+This will automatically open http://localhost:8080
+
+## Build
+
+```bash
+# build for test environment
+npm run build:stage
+
+# build for production environment
+npm run build:prod
 ```
 
-### Compiles and minifies for production
-```
-npm run build
-```
+## Advanced
 
-### Lints and fixes files
-```
+```bash
+# preview the release environment effect
+npm run preview
+
+# preview the release environment effect + static resource analysis
+npm run preview -- --report
+
+# code format check
 npm run lint
+
+# code format check and auto fix
+npm run lint -- --fix
 ```
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+
